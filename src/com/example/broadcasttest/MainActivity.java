989b74244971ea71e3de65a01303dc5c766adc6c
Heart-travel <zcgacbc@gmail.com>
2015-04-26 17:22:45 +0800

    Modify the code to monitor the status of network using the Broadcast

--- src/com/example/broadcasttest/MainActivity.java
+++ src/com/example/broadcasttest/MainActivity.java
@@ -1,18 +1,44 @@
 package com.example.broadcasttest;
 
 import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
+	
+	private IntentFilter intentFilter;
+	private NetworkChangeReceiver networkChangeReceiver;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		intentFilter = new IntentFilter();
+		intentFilter.addAction("android.net.conn.CONNECTIVITY_CHANGE");
+		networkChangeReceiver = new NetworkChangeReceiver();
+		registerReceiver(networkChangeReceiver, intentFilter);
 	}
-
+	
+	@Override
+	protected void onDestroy() {
+	super.onDestroy();
+		unregisterReceiver(networkChangeReceiver);
+	}
+	
+	class NetworkChangeReceiver extends BroadcastReceiver {
+		@Override
+		public void onReceive(Context context, Intent intent) {
+			Toast.makeText(context, "network changes", Toast.LENGTH_SHORT).show();
+		}
+	}
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
