ffd05015a2fb53bb5249508199b83996f0dfd659
Heart-travel <zcgacbc@gmail.com>
2015-04-26 21:19:38 +0800

    Add the available judgment of network

--- src/com/example/broadcasttest/MainActivity.java
+++ src/com/example/broadcasttest/MainActivity.java
@@ -5,6 +5,8 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -35,7 +37,18 @@ public class MainActivity extends Activity {
 	class NetworkChangeReceiver extends BroadcastReceiver {
 		@Override
 		public void onReceive(Context context, Intent intent) {
-			Toast.makeText(context, "network changes", Toast.LENGTH_SHORT).show();
+/*			Toast.makeText(context, "network changes", Toast.LENGTH_SHORT).show();*/
+			
+			ConnectivityManager connectionManager = (ConnectivityManager)getSystemService(Context.CONNECTIVITY_SERVICE);
+				NetworkInfo networkInfo = connectionManager.getActiveNetworkInfo();
+				
+				if (networkInfo != null && networkInfo.isAvailable()) {
+					Toast.makeText(context, "network is available",
+					Toast.LENGTH_SHORT).show();
+				} else {
+					Toast.makeText(context, "network is unavailable",
+					Toast.LENGTH_SHORT).show();
+				}
 		}
 	}
 	
